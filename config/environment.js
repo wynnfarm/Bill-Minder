1176b12f7546b3a8ed58e9755236e1232fb9c9e9
wynnfarm <wynnfarm@gmail.com>
2017-12-09 22:51:16 -0600

    Fixture working

--- config/environment.js
+++ config/environment.js
@@ -24,12 +24,18 @@ module.exports = function(environment) {
     }
   };
 
+  ENV['ember-cli-mirage'] = {
+    enabled: false
+  };
+
+
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
+    ENV['ember-cli-mirage'].enabled = true;
   }
 
   if (environment === 'test') {
