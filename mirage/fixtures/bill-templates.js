f9953ae5bef23efbbde4758795e0333462cafabd
wynnfarm <wynnfarm@gmail.com>
2017-12-09 22:51:24 -0600

    ditto

--- mirage/fixtures/bill-templates.js
+++ mirage/fixtures/bill-templates.js
@@ -1,6 +1,6 @@
 export default [{
   id: '1',
-  type: 'bill',
+  type: 'bill-template',
   billName: 'AmerenIP',
   account: 'BillPay Checking',
   billAmount: '120.00',
@@ -9,7 +9,7 @@ export default [{
   frequency: 'monthly'
 }, {
   id: '2',
-  type: 'bill',
+  type: 'bill-template',
   billName: 'Mazda License',
   account: 'General Checking',
   billAmount: '103.00',
@@ -18,7 +18,52 @@ export default [{
   frequency: 'yearly'
 }, {
   id: 3,
-  type: 'bill',
+  type: 'bill-template',
+  billName: 'Mortgage',
+  account: 'BillPay Checking',
+  billAmount: '501.00',
+  startDate: '1/31/2017',
+  endDate: '2/13/2017',
+  frequency: 'bi-weekly'
+},{
+  id: 4,
+  type: 'bill-template',
+  billName: 'American Pest Control',
+  account: 'BillPay Checking',
+  billAmount: '75.00',
+  startDate: '1/31/2017',
+  endDate: '2/13/2017',
+  frequency: 'bi-weekly'
+},{
+  id: 5,
+  type: 'bill-template',
+  billName: 'Mortgage',
+  account: 'BillPay Checking',
+  billAmount: '501.00',
+  startDate: '1/31/2017',
+  endDate: '2/13/2017',
+  frequency: 'bi-weekly'
+},{
+  id: 6,
+  type: 'bill-template',
+  billName: 'Mortgage',
+  account: 'BillPay Checking',
+  billAmount: '501.00',
+  startDate: '1/31/2017',
+  endDate: '2/13/2017',
+  frequency: 'bi-weekly'
+},{
+  id: 7,
+  type: 'bill-template',
+  billName: 'Mortgage',
+  account: 'BillPay Checking',
+  billAmount: '501.00',
+  startDate: '1/31/2017',
+  endDate: '2/13/2017',
+  frequency: 'bi-weekly'
+},{
+  id: 8,
+  type: 'bill-template',
   billName: 'Mortgage',
   account: 'BillPay Checking',
   billAmount: '501.00',
